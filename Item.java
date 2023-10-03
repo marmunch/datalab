ba4edf8218078dd6068adafc6609f008f4818b37
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-03 12:42:13 +0300

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,19 @@
+package Item;
+public class Item {
+    	
+    	private char[] address;
+    	private char[] name;
+
+    	Item() {
+    		this.address = null;
+    		this.name = null;
+    	}
+    	
+    	Item(char[] address, char[] name) {
+    		this.address = ;
+    		this.name = name;
+    		// скопировать посимвольно 
+    	}
+    	
+    	// equals
+    }
