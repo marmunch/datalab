90dd6433510fa999494628369f773f0cef57a174
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-03 12:41:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
+import List.ATD;
+import List.ATD.Position;
 public class Main {
 
 	public static void deleteDuplicates() {
-		/*
-		 * 
-		 */
+		ATD L;
+		Position p, q;
+		p = L.first();
+		while(p != L.end()) {
+			q = L.next(p);
+			while(q != L.end()) {
+				if(/*сравнение*/)
+					L.delete(q);
+				else q = L.next(q);
+			}
+			p = L.next(p);
+		}
 	}
 	
     public static void main(String[] args) {
