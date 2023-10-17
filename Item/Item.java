dd90d970e28c7f982112e4bd832de17a795f5b5a
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-17 14:04:53 +0300

    Update Item.java

--- Item/Item.java
+++ Item/Item.java
@@ -6,29 +6,39 @@ public class Item {
     private char[] name;
 
     // конструктор
-    Item() {
+    public Item() {
         this.address = null;
         this.name = null;
     }
 
     // копирующий конструктор
-    public Item(char[] address, char[] name) {
+    public Item(Item item) { // сделать item
         this.address = new char[50];
         this.name = new char[30];
 
         int i;
         for(i = 0; i < 30; i++) {
-            this.address[i] = address[i];
-            this.name[i] = name[i];
+            this.address[i] = item.address[i];
+            this.name[i] = item.name[i];
         }
         for(i = 30; i < 50; i++) {
-            this.address[i] = address[i];
+            this.address[i] = item.address[i];
         }
     }
 
     // сравнение объектов
-    public boolean equals(Item other) {
-        if(this.address.equals(other.address) && this.name.equals(other.name)) return true;
-        else return false;
+    public boolean equals(Item other) {//написать самой
+        int i;
+        for(i = 0; i < 30; i++) {
+            if(this.address[i] != other.address[i] && this.name[i] != other.name[i]) return false;
+        }
+        for(i = 30; i < 50; i++) {
+            if(this.address[i] != other.address[i]) return false;
+        }
+        return true;
+    }
+
+    public String toString() {
+        return "___Name: " + new String(name) + "; ___Address: " + new String(address);
     }
 }
