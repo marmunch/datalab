bd98455731d83c7304a845b779aa16ff061e3203
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-17 10:13:25 +0300

    Update Item.java

--- Item/Item.java
+++ Item/Item.java
@@ -1,30 +1,34 @@
 package Item;
-public class Item {
-    	
-    	private char[] address;
-    	private char[] name;
 
-    	Item() {
-    		this.address = null;
-    		this.name = null;
-    	}
-    	
-    	Item(char[] address, char[] name) {
-    		this.address = new char[50];
-    		this.name = new char[30];
-    		
-    		int i;
-    		for(i = 0; i < 30; i++) {
-    			this.address[i] = address[i];
-    			this.name[i] = name[i];
-    		}
-    		for(i = 30; i < 50; i++) {
-    			this.address[i] = address[i];
-    		}
-    	}
-    	
-    	public boolean equals(Item other) {
-    		if(this.address.equals(other.address) && this.name.equals(other.name)) return true;
-    		else return false;
-    	}
+public class Item {
+
+    private char[] address;
+    private char[] name;
+
+    // конструктор
+    Item() {
+        this.address = null;
+        this.name = null;
     }
+
+    // копирующий конструктор
+    public Item(char[] address, char[] name) {
+        this.address = new char[50];
+        this.name = new char[30];
+
+        int i;
+        for(i = 0; i < 30; i++) {
+            this.address[i] = address[i];
+            this.name[i] = name[i];
+        }
+        for(i = 30; i < 50; i++) {
+            this.address[i] = address[i];
+        }
+    }
+
+    // сравнение объектов
+    public boolean equals(Item other) {
+        if(this.address.equals(other.address) && this.name.equals(other.name)) return true;
+        else return false;
+    }
+}
