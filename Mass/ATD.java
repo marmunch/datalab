2e94d872498f6a394072171af0730a56bfaa9d0f
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-17 10:12:54 +0300

    Update ATD.java

--- Mass/ATD.java
+++ Mass/ATD.java
@@ -3,112 +3,133 @@ import Item.Item;
 
 public class ATD {
 
-	private Item[] mass;
-	
-	ATD() {
+    private Item[] mass;
+    private int size = 10;
+    private int last;
+
+    // конструктор
+    ATD() {
         /*
         создать массив
          */
-    	this.mass = new Item[10];
+        this.mass = null;
+        last = 0;
     }
-	
+
+    // конструктор
     ATD(char[] adr, char[] name) {
         /*
         создать массив
          */
-    	this.mass = new Item[10];
-    	mass[0] = new Item(adr, name);
+        this.mass = new Item[size];
+        mass[0] = new Item(adr, name);
     }
 
-    private int end() {
+    // конец массива
+    public Position end() {
         /*
-        пока данный object не null(элемент массива), переходить на следующий
-        вернуть позицию
+        вернуть позицию последнего
+         */
+        return new Position(last);
+    }
+
+    // вставка
+    public void insert(Item x, Position p) {
+        /*
+         * вставка в начало или середину (Position < size и > 0)
+         *      ищем позицию (equals)
+         *      позиция найдена - метод move_right() с Position до end
+         * вставка в конец
+         *      mass[last] = new Item(x);
+         *      last++;
          */
     }
 
-    private void insert(char[] x, int p) {
+    // двинуть массив вправо
+    private void move_right(int start, int end, Item time1) {
         /*
-         * p > 0 and p < 10
-        если позиция есть:
-            ищем элемент с позицией p (цикл с 0 до p)
-            вставляем элемент, элемент с номером p запомнить в временную переменную t
-            в цикле остальные двигаем вправо (с p+1 до end())
-        если нет - ничего не делать
+         * цикл с start до last-1 (переставляю элементы вправо по парам)
+         *      time2 = mas[i+1];
+         *      mas[i+1] = mas[i];
+         *      mas[i] = time1;
+         *      time1 = time2;
          */
     }
 
-    private int locate(char[] x) {
+    // вернуть
+    public Position locate(char[] x) {
         /*
-        идем по списку с first
-            если объект есть - вернуть позицию
-        если объекта нет - вернуть end()
+         * идти с 0 до last
+         *      проверка элемента массива через equals
+         *          true - вернуть позицию
+         * элемент не найден - вернуть null
          */
     }
 
-    private char[] retrieve(int p) throws Exception{
+    public Item retrieve(Position p) {
         /*
-         * p > 0 and p < 10
-        идем по списку
-            если находится элемент, то возвращаем его
-        элемент не найден или p = end(), выбросить исключение
+         * проверить позицию check_pos(p)
+         *      c 0 до last
+         *          проверка позиции через equals
+         *              true - вернуть копию объекта
+         * элемент не найден либо check_pos вернуло false - выбросить исключение
          */
     }
 
-    private void delete(int p) {
+    public void delete(Position p) {
         /*
-         * p > 0 and p < 10
-        идем по списку
-            находим элемент, двигаем на него все элементы справа
-        в списке нет элемента - ничего не делать
+         * проверить позицию check_pos(p)
+         * если удаление первого или середины метод move_left(), переставить позицию
+         * если конец то last-- и переставить позицию;
          */
     }
 
-    private int next(int p) {
+    // двинуть массив влево
+    private void move_left(int start, int end) {
         /*
-         * p > 0 and p < 10
-        вернуть следующую позицию
+         * цикл с start до last-1 (переставляю элементы влево по парам)
+         *      mas[i] = mas[i+1];
+         * last--;
          */
     }
 
-    private int previous(int p) {
+    public Position next(Position p) {
         /*
-         * p > 0 and p < 10
-        вернуть предыдущую позицию
+         * проверить позицию check_pos(p)
+         * вернуть позицию p+1
          */
     }
 
-    private void makenull() {
+    public Position previous(Position p) {
         /*
-        сделать null
-         */
-    	this.mass = new Item[10];
-    }
-
-    private int first() {
-        /*
-        вернуть первый если массив не пуст
+         * проверить позицию check_pos(p)
+         * вернуть позицию p-1
          */
     }
 
-    private void printlist() {
+    public void makenull() {
         /*
-        идти по списку и вывести на печать каждый объект имя + адрес
+        сделать last = 0
+         */
+        last = 0;
+    }
+
+    private boolean check_pos(Position p) {
+        /*
+         * если позиция > 0 и < last вернуть true
+         * иначе false
          */
     }
-    
-    private class Position {
 
-    	int p;
-    	
-    	Position(int p) {
-    		this.p = p;
-    	}
-    	
-    	private boolean equals(int q) {
-    		
-    		if(q.p == this.p) return true;
-    		else return false;
-    	}
+    public Position first() {
+        /*
+        вернуть 0 если массив не пуст
+         */
+    }
+
+    public void printlist() {
+        /*
+        идти по списку и вывести на печать каждый объект (имя + адрес)
+         */
     }
 }
