2ce61d807908dcac8e4590c32a5413b17c40f3a4
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-31 12:22:09 +0300

    Update Item.java

--- Item/Item.java
+++ Item/Item.java
@@ -11,17 +11,31 @@ public class Item {
         this.name = null;
     }
 
-    // копирующий конструктор
-    public Item(Item item) { // сделать item
-        this.address = new char[50];
-        this.name = new char[30];
+    // конструктор
+    public Item(char[] adr, char[] nm) {
+
+        this.address = new char[adr.length];
+        this.name = new char[nm.length];
 
         int i;
-        for(i = 0; i < 30; i++) {
-            this.address[i] = item.address[i];
+        for(i = 0; i < nm.length; i++) {
+            this.name[i] = nm[i];
+        }
+        for(i = 0; i < adr.length; i++) {
+            this.address[i] = adr[i];
+        }
+    }
+
+    // копирующий конструктор
+    public Item(Item item) { // сделать item
+        this.address = new char[item.address.length];
+        this.name = new char[item.name.length];
+
+        int i;
+        for(i = 0; i < item.name.length; i++) {
             this.name[i] = item.name[i];
         }
-        for(i = 30; i < 50; i++) {
+        for(i = 0; i < item.address.length; i++) {
             this.address[i] = item.address[i];
         }
     }
@@ -29,10 +43,10 @@ public class Item {
     // сравнение объектов
     public boolean equals(Item other) {//написать самой
         int i;
-        for(i = 0; i < 30; i++) {
-            if(this.address[i] != other.address[i] && this.name[i] != other.name[i]) return false;
+        for(i = 0; i < name.length && i < other.name.length; i++) {
+            if(this.name[i] != other.name[i]) return false;
         }
-        for(i = 30; i < 50; i++) {
+        for(i = 0; i < address.length && i < other.address.length; i++) {
             if(this.address[i] != other.address[i]) return false;
         }
         return true;
