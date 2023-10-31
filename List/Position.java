25469de684769da5c132de087cf7ac4a5bf1d450
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-31 12:21:57 +0300

    Update Position.java

--- List/Position.java
+++ List/Position.java
@@ -7,13 +7,22 @@ public class Position {
     protected Node p;  // нод
 
     // конструктор
-    Position(Node p) {
+    public Position(Node p) {
         this.p = p;
     }
 
     // сравнение
-    protected boolean equals(Position q) {
+    public boolean equal(Position q) {
+        //System.out.println('1');
         if(this == q) return true;
-        return q.p == this.p;
+        //System.out.println('2');
+        if(this.p != null) return this.p.equals(q.p);
+        else if(q.p == null) return true;
+        else return false;
+    }
+
+    public void print() {
+        if (p != null) p.print();
+        else System.out.println("Node is null");
     }
 }
