3cb50252f7fff124e51aa105f22cab816154f535
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-31 12:21:46 +0300

    Update ATD.java

--- List/ATD.java
+++ List/ATD.java
@@ -6,10 +6,7 @@ public class ATD {
     private Node head; // голова списка
 
     // конструктор
-    ATD() {
-        /*
-        создать узел
-         */
+    public ATD() {
         this.head = null;
     }
 
@@ -36,11 +33,60 @@ public class ATD {
          * если вставка в конец:
          * 		присоединить новый нод в конец
          */
+
+        if (head == null) {
+            head = new Node(x);
+            //System.out.println('1');
+        }
+        else if (p == null || p.p == null) {
+            Node cur = findNodePrev(head, null);
+            //cur.print();
+            cur.next = new Node(x);
+            //cur.next.print();
+            //System.out.println('2');
+        }
+        else if (head.item.equals(p.p.item)) {
+            Node node = new Node(x);
+            Node prev = new Node(head);
+            head = node;
+            //head.print();
+            head.next = prev;
+            //head.next.print();
+            //System.out.println('3');
+        }
+        else {
+            Node prev = findNodePrev(head, p.p);
+            //prev.print();
+            Node cur = findNode(head, p.p);
+            //p.p.print();
+            //cur.print();
+            if(cur.item.equals(p.p.item)) {
+                prev.next = new Node(x);
+                prev = prev.next;
+                prev.next = cur;
+                //System.out.println('5');
+            }
+            //System.out.println('4');
+        }
     }
 
     // находить последний нод
+    private Node findNode(Node current, Node tofind) {
+
+        while(current != null && !current.equals(tofind)) {
+            current = current.next;
+        }
+        return current;
+    }
 
     // метод поиска позиции возвращать предыдущую (проверять head перед методом)
+    private Node findNodePrev(Node current, Node end) {
+
+        while(current.next != end) {
+            current = current.next;
+        }
+        return current;
+    }
 
     // вернуть позицию по объекту
     public Position locate(Item x) {
@@ -49,6 +95,14 @@ public class ATD {
             если объект равен х по equals - вернуть позицию
         если объекта нет(цикл завершил свою работу и объект не нашел) - вернуть null
          */
+
+        Node current = head;
+        while (current != null) {
+            //current.print();
+            if (x.equals(current.item)) return new Position(current);
+            current = current.next;
+        }
+        return new Position(null);
     }
 
     // вернуть объект в позиции
@@ -60,6 +114,15 @@ public class ATD {
          * иначе выбросить исключение
          IndexOutOfBoundsException
          */
+
+        if (p != null) {
+            Node current = head;
+            while(current != null) {
+                if (current.equals(p.p)) return new Item(current.item);
+                current = current.next;
+            }
+        }
+        throw new IndexOutOfBoundsException();
     }
 
     // удалить объект в позиции
@@ -74,6 +137,29 @@ public class ATD {
          * 		элемент не найден - ничего не делать
          * иначе ничего не делать
          */
+
+        if (p.p == head) {
+            head = head.next;
+            //System.out.println('1');
+        }
+        else if (p != null) {
+            Node current = head.next;
+            //current.print();
+            Node time = head;
+            //time.print();
+            //p.p.print();
+            while (current != null) {
+                //System.out.println(p.p == current);
+                //current.print();
+                if (p.p == current) {
+                    time.next = current.next;
+                }
+                time = time.next;
+                current = current.next;
+            }
+            //System.out.println('2');
+        }
+        //System.out.println('3');
     }
 
     // вернуть последующую позицию
@@ -82,8 +168,8 @@ public class ATD {
          * если (p != null)
          * вернуть следующую позицию
          */
-        if(p != null) return new Position(p.p.next);
-        else return null;
+        if(p.p != null) return new Position(p.p.next);
+        else return new Position(null);
     }
 
     // вернуть предыдущую позицию
@@ -96,6 +182,16 @@ public class ATD {
          *      в time сохраняем позицию данного нода
          * позиции нет - вернуть null
          */
+
+        if (p.p.equals(head)) {
+            return new Position(null);
+        }
+        else {
+            Node current = findNode(head, p.p);
+            Node prev = findNodePrev(head, current);
+            if (current.equals(p.p)) return new Position(prev);
+            else return new Position(null);
+        }
     }
 
     // обнулить список
@@ -111,7 +207,7 @@ public class ATD {
 
         if(head != null)
             return new Position(head);
-        else return null;
+        else return new Position(null);
     }
 
     // вывести на экран
@@ -119,28 +215,47 @@ public class ATD {
         /*
         идти по списку и вывести на печать каждый узел
          */
-        Node current = head;
-        while(current != null) {
-
-            current = current.next;
+        if (head == null) System.out.println("List is empty");
+        else {
+            Node current = head;
+            while (current != null) {
+                System.out.println(current.item.toString());
+                current = current.next;
+            }
         }
     }
 
     // класс нода
     protected class Node {
 
-        Item item;
+        protected Item item;
         private Node next;
 
         // конструктор
-        Node() {
+        protected Node() {
             this.next = null;
         }
 
+        // конструктор по Item
+        protected Node(Item item) {
+            this.item = new Item(item);
+        }
+
         // копирующий конструктор
-        Node(Node copy) {
+        protected Node(Node copy) {
             this.item = new Item(copy.item);
             this.next = copy.next;
+        }
+
+        protected void print() {
+
+            if (item != null) System.out.println(item);
+            else System.out.println("Item is null");
+        }
+
+        protected boolean equals(Node node) {
+            if(node != null) return this.item.equals(node.item);
+            return false;
         }
     }
 }
