dd8dbb847d1dbb9f7f5fb1e10210e5eeeb435e77
marmunch <116271998+marmunch@users.noreply.github.com>
2023-10-31 12:22:35 +0300

    Update ATD.java

--- Mass/ATD.java
+++ Mass/ATD.java
@@ -8,13 +8,13 @@ public class ATD {
     private int last;  // элемент после последнего занятого
 
     // конструктор
-    ATD() {
+    public ATD() {
         this.mass = null;
         last = 0;
     }
 
     // конструктор
-    ATD(Item item) {
+    public ATD(Item item) {
         /*
         создать массив
          */
@@ -41,22 +41,29 @@ public class ATD {
          *      last++;
          */
         int pos = 0;
-        if(p.p > 0 && p.p < last) {
-            for(int i=0; i<last; i++) {
-                if(p.p == i) {
-                    pos = i;
-                    break;
-                }
-            }
+        if (mass == null) {
+            mass = new Item[size];
+            mass[0] = new Item(x);
+        }
+        else if(checkPos(p)) {
+            pos = findPos(p.p);
             for(int i=last; i>pos; i--) {
                 mass[i] = mass[i-1];
             }
             mass[pos] = new Item(x);
         }
-        if(p.p == last) {
+        else if(p.p == last) {
             mass[last] = new Item(x);
         }
         last++;
+    }
+
+    // поиск
+    private int findPos(int it) {
+        for(int i=0; i<last; i++) {
+            if(it == i) return i;
+        }
+        return last;
     }
 
     // вернуть
@@ -70,7 +77,7 @@ public class ATD {
         for(int i=0; i<last; i++) {
             if(x.equals(mass[i])) return new Position(i);
         }
-        return null;
+        return new Position(last);
     }
 
     public Item retrieve(Position p) {
@@ -81,21 +88,25 @@ public class ATD {
          *              true - вернуть копию объекта
          * элемент не найден либо check_pos вернуло false - выбросить исключение
          */
-        if(p.p >= 0 && p.p < last) {
-            for(int i=0; i< last; i++) {
-                if(p.p == i) return new Item(mass[i]);
-            }
+        //System.out.println(p.p);
+        if(checkPos(p)) {
+            int pos = findPos(p.p);
+            return new Item(mass[pos]);
         }
         throw new IndexOutOfBoundsException();
     }
 
+    private boolean checkPos(Position p) {
+        return (p.p >= 0 && p.p < last);
+    }
+
     public void delete(Position p) {
         /*
-         * проверить позицию check_pos(p)
+         * проверить позицию
          * если удаление первого или середины метод move_left(), переставить позицию
          * если конец то last-- и переставить позицию;
          */
-        if(p.p >= 0 && p.p < last) {
+        if(checkPos(p)) {
             for(int i=p.p; i<last-1; i++) {
                 mass[i] = mass[i+1];
             }
@@ -108,8 +119,9 @@ public class ATD {
          * проверить позицию check_pos(p)
          * вернуть позицию p+1
          */
-        if(p.p >= 0 && p.p <= last) return new Position(p.p+1);
-        else return null;
+        //p.print();
+        if(checkPos(p)) return new Position(p.p+1);
+        else return new Position(last);
     }
 
     public Position previous(Position p) {
@@ -118,7 +130,7 @@ public class ATD {
          * вернуть позицию p-1
          */
         if(p.p >= 0 && p.p <= last) return new Position(p.p-1);
-        else return null;
+        else return new Position(last);
     }
 
     public void makenull() {
@@ -133,7 +145,7 @@ public class ATD {
         вернуть 0 если массив не пуст
          */
         if(mass != null) return new Position(0);
-        else return null;
+        else return new Position(last);
     }
 
     public void printlist() {
