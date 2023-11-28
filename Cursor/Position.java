0ee0a2b87c82d563f6cc4bee2810cdc20ef55740
marmunch <116271998+marmunch@users.noreply.github.com>
2023-11-28 12:58:36 +0300

    Update Position.java

--- Cursor/Position.java
+++ Cursor/Position.java
@@ -1,2 +1,21 @@
 package Cursor;
 
+public class Position {
+
+    protected int p; // позиция
+
+    // конструктор
+    Position(int p) {
+        this.p = p;
+    }
+
+    // сравнение
+    public boolean equal(Position q) {
+
+        if(q.p == this.p) return true;
+        else return false;
+    }
+
+    // вывести позицию
+    public void print() { System.out.println(p); }
+}
