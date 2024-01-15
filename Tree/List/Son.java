e79327e27f459b6a0ba6e243e56ede68cce3f81e
marmunch <116271998+marmunch@users.noreply.github.com>
2024-01-15 15:32:26 +0300

    Create Son.java

---
+++ Tree/List/Son.java
@@ -0,0 +1,14 @@
+package Tree.List;
+
+public class Son {
+
+    public int label;
+    public Son next;
+
+    // конструктор
+    public Son(int label, Son next) {
+
+        this.label = label;
+        this.next = next;
+    }
+}
